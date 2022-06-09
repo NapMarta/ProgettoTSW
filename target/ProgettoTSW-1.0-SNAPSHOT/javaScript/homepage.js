6db27af6c2e88a7e73ff1f97925e141ecbd86bd8
NapMarta <marta.napolillo@outlook.it>
2022-06-09 09:29:30 +0200

    Update Servlet

---
+++ target/ProgettoTSW-1.0-SNAPSHOT/javaScript/homepage.js
@@ -0,0 +1,17 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+/*
+function dropUpUtente() {
+    document.getElementById("elements").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+        var elements = document.getElementById("elements");
+        if (elements.classList.contains('show')) {
+            elements.classList.remove('show');
+        }
+    }
+}
+*/
