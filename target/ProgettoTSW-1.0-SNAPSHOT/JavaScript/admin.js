bc00c4911ca159e8f9bd39601fd6d9539292e002
simoLoc <simonaloc1@yahoo.it>
2022-06-15 12:48:17 +0200

    modifica css

---
+++ target/ProgettoTSW-1.0-SNAPSHOT/JavaScript/admin.js
@@ -0,0 +1,9 @@
+function menu() {
+    var x = document.getElementById("Topnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
